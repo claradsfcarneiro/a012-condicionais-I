a8df0fb7205d3b2f822e7b516ee30855b9cd1f69
Clara <claradsfcarneiro@gmail.com>
2022-09-06 20:52:58 -0300

    Realizar exercício 1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,7 @@
-const num = //seu numero aqui
+const num = Number(prompt("Qual é o seu número?"));
 
-if(){
-    console.log("sua mensagem aqui");
+if(num % 2 === 0){
+    console.log("O número é par");
+} else {
+    console.log("O número é ímpar");
 }
