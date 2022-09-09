f91c51e6f3aafaf8c290f852e5e1a48777f1a9ba
Clara <claradsfcarneiro@gmail.com>
2022-09-09 20:08:33 -0300

    Realizar exercício 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,22 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = Number(prompt("Qual é a sua idade?"));
+let terminouEnsinoMedio = String(prompt("Você terminou o ensino médio?")).toLowerCase() === "sim";
+let cursandoFaculdade = String(prompt("Você está cursando uma faculdade?")).toLowerCase() === "sim";
 
-if(){
+if(idade >=18 === true){
+    console.log("Você é maior de idade")
 
 }else{
-	
+    console.log("Você não é maior de idade");
+}
+
+if(terminouEnsinoMedio === true){
+    console.log("Você terminou o ensino médio");
+}else{
+    console.log("Você não terminou o ensino médio");
+}
+
+if(cursandoFaculdade === false){
+    console.log("Você não está cursando uma faculdade");
+}else{
+    console.log("Você está cursando uma faculdade")
 }
